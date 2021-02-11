83005297c0e1650bd68257daae36f9560ccc7f3d
sfdevelop <sfdevelop747@gmail.com>
2021-02-11 23:06:31 +0200

    уведомления

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,7 @@
 //= ../../../node_modules/bootstrap/js/dist/util.js
 //= ../../../node_modules/bootstrap/js/dist/dropdown.js
 //= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/toast.js
 
 //= ../../../bower_components/slick-carousel/slick/slick.js
 
