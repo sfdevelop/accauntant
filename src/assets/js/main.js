c259cb983a9d44bb9e8ae3edcde276dbb723281c
sfdevelop <sfdevelop747@gmail.com>
2021-02-25 13:54:26 +0200

    модальное окно

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,7 @@
 //= ../../../node_modules/bootstrap/js/dist/dropdown.js
 //= ../../../node_modules/bootstrap/js/dist/collapse.js
 //= ../../../node_modules/bootstrap/js/dist/toast.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
 
 //= ../../../bower_components/slick-carousel/slick/slick.js
 
